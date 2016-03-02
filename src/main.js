7137df05c4a56e9eef8773fd3a0811ef75037b66
Julien Déray <julien@deray.fr>
2016-03-02 18:43:13 +0000

    implements grade transformation

--- src/main.js
+++ src/main.js
@@ -17,14 +17,13 @@ var fs = require('fs');
 var parse = require('csv-parse');
 var transform = require('stream-transform');
 
-let columns = [];
+let transformedColumns;
 
 var parser = parse({
     delimiter: ',',
     columns: firstLine => {
-        columns = transformColumns(firstLine);
-        console.log(columns);
-        return columns;
+        transformedColumns = transformColumns(JSON.parse(JSON.stringify(firstLine)));
+        return firstLine;
     }
 });
 
@@ -51,12 +50,29 @@ function transformColumns(columns) {
     let gradeIndex = columns.indexOf('grade');
     const columnsToAdd = gradesList.map(grade => `grade_${grade}`);
     columnsToAdd.forEach(col => columns.splice(gradeIndex++, 0, col));
+    columns.splice(gradeIndex, 1);
     return columns;
 }
 
 function transformGrades(record) {
+    console.log(record);
     const gradesResult = Array(gradesList.length).fill(0);
     const gradeNo = gradesList.indexOf(record.grade);
     gradesResult[gradeNo] = 1;
-    return gradesResult.concat([]).join(' ') + '\n';
+
+    const recordKeys = Object.keys(record);
+    const gradeColIndex = recordKeys.indexOf('grade');
+
+    let result = [];
+
+    recordKeys.forEach((key, index) => {
+        if (index == gradeColIndex) {
+            result.push(gradesResult);
+        }
+        else {
+            result.push(record[key]);
+        }
+    });
+
+    return result + '\n';
 }
