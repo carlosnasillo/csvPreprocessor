67af6d08bcc27d47aea400684808caaf546cedcd
Julien Déray <julien@deray.fr>
2016-03-02 18:24:17 +0000

    transforms grade in columns

--- src/main.js
+++ src/main.js
@@ -3,13 +3,32 @@
 * Created on 02/03/2016
 */
 
+"use strict";
+
+/**
+ * Constants
+ */
+const gradesList = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+/**
+ * Main
+ */
 var fs = require('fs');
 var parse = require('csv-parse');
 var transform = require('stream-transform');
 
-var output = [];
-var parser = parse({delimiter: ':', columns: true});
-var input = fs.createReadStream('csv/test.csv');
+let columns = [];
+
+var parser = parse({
+    delimiter: ',',
+    columns: firstLine => {
+        columns = transformColumns(firstLine);
+        console.log(columns);
+        return columns;
+    }
+});
+
+var input = fs.createReadStream('csv/LCmerged.csv');
 
 var transformer = transform(function(record, callback){
     setTimeout(function(){
@@ -20,16 +39,24 @@ var transformer = transform(function(record, callback){
 input.pipe(parser).pipe(transformer).pipe(process.stdout);
 
 
-
+/**
+ * Functions
+ */
 
 function transformRecord(record) {
     return transformGrades(record);
 }
 
+function transformColumns(columns) {
+    let gradeIndex = columns.indexOf('grade');
+    const columnsToAdd = gradesList.map(grade => `grade_${grade}`);
+    columnsToAdd.forEach(col => columns.splice(gradeIndex++, 0, col));
+    return columns;
+}
+
 function transformGrades(record) {
-    const gradesList = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
     const gradesResult = Array(gradesList.length).fill(0);
     const gradeNo = gradesList.indexOf(record.grade);
     gradesResult[gradeNo] = 1;
-    return gradesResult.concat([record.name]).join(' ') + '\n';
+    return gradesResult.concat([]).join(' ') + '\n';
 }
