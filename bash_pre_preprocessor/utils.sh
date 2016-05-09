057c54a2f4d458bb4c314d9d16778b83dd1be4fc
Julien Déray <julien@deray.fr>
2016-05-09 16:10:29 +0100

    update csv name

--- bash_pre_preprocessor/utils.sh
+++ bash_pre_preprocessor/utils.sh
@@ -1,8 +1,12 @@
-FILE=LoanStats3b_securev1.csv
+FILE="LoanStats3a_securev1.csv"
 
 # remove invalid loans (after the text "Loans that do not meet the credit policy")
 head -`grep -n "Loans that do not meet the credit policy" $FILE | awk -F ":" '{print $1}'` $FILE  > ${FILE}.valid
 # remove the 3 last lines
 sed '$d' $FILE.valid | sed '$d' | sed '$d' > ${FILE}.good
+
+# remove first line of the FILE
+sed '1d' $FILE
+
 # remove all double quotes (csv import into db)
-gsed -i 's/"//g' preprocessed.csv
+gsed -i 's/"//g' concat_preprocessed.csv
